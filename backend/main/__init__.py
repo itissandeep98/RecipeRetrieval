fd6ab8af88ef2909a5ed5f092ecbba627deb75eb
itissandeep98 <itissandeep98@gmail.com>
2021-05-09 12:05:06 +0530

    directory structure change

--- backend/main/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-app.secret_key = '5ZN5zi!45QUsGG'
-
-from main import routes
--- frontend/backend.sh
+++ frontend/backend.sh
@@ -1,4 +1,3 @@
 cd ../backend
 source venv/bin/activate
-pip install -r requirements.txt
 python3 run.py
